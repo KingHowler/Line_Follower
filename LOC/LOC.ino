d6ea869ecc02a972428038ac5b9dc5cd8a05a8e0
M. Umar Shahbaz <68814294+KingHowler@users.noreply.github.com>
2024-08-08 23:08:49 +0500

    Sped up main loop runtime duration
    Used data containers to take readings only once per iteration. This will drastically reduce runtime and hence improve accuracy and reaction time.

--- LOC/LOC.ino
+++ LOC/LOC.ino
@@ -1,10 +1,17 @@
-int LSensor=2;
-int RSensor=13;
-int CSensor=7;
-int LMotor_1=3;
-int LMotor_2=5;
-int RMotor_1=6;
-int RMotor_2=9;
+// Using "const" for assigning pin numbers is a good practice as it tells the computer that they don't need to be written to
+const int LSensor=2;
+const int RSensor=13;
+const int CSensor=7;
+const int LMotor_1=3;
+const int LMotor_2=5;
+const int RMotor_1=6;
+const int RMotor_2=9;
+
+// You should use containers for readings and update them only once per loop This will drastically improve performence as each iteration is much faster
+int LSensorReading;
+int RSensorReading;
+int CSensorReading;
+
 void setup() {
   pinMode(LSensor,INPUT);
   pinMode(RSensor,INPUT);
@@ -16,21 +23,25 @@ void setup() {
 }
 
 void loop() {
-  if (digitalRead(CSensor)==0 && digitalRead(LSensor)==1 && digitalRead(RSensor)==1)
+  LSensorReading = digitalRead(LSensor);
+  RSensorReading = digitalRead(RSensor);
+  CSensorReading = digitalRead(CSensor);
+  
+  if (CSensorReading==0 && LSensorReading==1 && RSensorReading==1)
   {
     analogWrite(LMotor_1,200);
     analogWrite(LMotor_2,0);
     analogWrite(RMotor_1,200);
     analogWrite(RMotor_2,0);
   }
-  else if (digitalRead(CSensor)==0 && digitalRead(LSensor)==0 && digitalRead(RSensor)==1)
+  else if (CSensorReading==0 && LSensorReading==0 && RSensorReading==1)
   {
     analogWrite(LMotor_1,0);
     analogWrite(LMotor_2,200);
     analogWrite(RMotor_1,200);
     analogWrite(RMotor_2,0);
   }
-  else if (digitalRead(CSensor)==0 && digitalRead(LSensor)==1 && digitalRead(RSensor)==0)
+  else if (CSensorReading==0 && LSensorReading==1 && RSensorReading==0)
   {
     analogWrite(LMotor_1,200);
     analogWrite(LMotor_2,0);
